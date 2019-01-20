d5f6b37705700be1ba7d149c34bcfcb7d6425494
AUSB <38762233+AUSB@users.noreply.github.com>
2019-01-19 22:40:34 -0500

    Update arrayandloop.java

--- arrayandloop.java
+++ arrayandloop.java
@@ -893,8 +893,77 @@ public class Lab0 {
 	
 	}
 		
-		
-		
 
+}
+
+
+package algorithms.shuffle;(array)
+
+import java.util.Random;
+
+public class ShuffleArray {
+    public static void shuffleArray(int[] a) {
+        int n = a.length;
+        Random random = new Random();
+        random.nextInt();
+        for (int i = 0; i < n; i++) {
+            int change = i + random.nextInt(n - i);
+            swap(a, i, change);
+        }
+    }
+
+    private static void swap(int[] a, int i, int change) {
+        int helper = a[i];
+        a[i] = a[change];
+        a[change] = helper;
+    }
+
+    public static void main(String[] args) {
+        int[] a = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+        shuffleArray(a);
+        for (int i : a) {
+            System.out.println(i);
+        }
+    }
+}
+Create the following program for sorting list.
+
+package algorithms.shuffle;(list)
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class ShuffleList {
+    public static void shuffleList(List<Integer> a) {
+        int n = a.size();
+        Random random = new Random();
+        random.nextInt();
+        for (int i = 0; i < n; i++) {
+            int change = i + random.nextInt(n - i);
+            swap(a, i, change);
+        }
+    }
+
+    private static void swap(List<Integer> a, int i, int change) {
+        int helper = a.get(i);
+        a.set(i, a.get(change));
+        a.set(change, helper);
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(7);
+        shuffleList(list);
+        for (int i : list) {
+            System.out.println(i);
+        }
+    }
 }
 
